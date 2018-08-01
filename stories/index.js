8d4c29ffc6f297578533d6201fa498f49459b5d4
Emerson Laurentino <emersonlaurentino@hotmail.com>
2018-07-31 22:26:43 -0300

    feat(stories): add pointing examples

--- stories/index.js
+++ stories/index.js
@@ -5,61 +5,108 @@ import 'semantic-ui-css/semantic.min.css';
 import SemanticDatepicker from '../src';
 import localePtBr from '../src/locales/pt-BR';
 
+const Content = ({ children }) => (
+  <div style={{ display: 'flex', flex: 1, justifyContent: 'center' }}>
+    {children}
+  </div>
+);
+
 storiesOf('Examples', module)
-  .add('Basic', () => <SemanticDatepicker onDateChange={console.log} />)
+  .add('Basic', () => (
+    <Content>
+      <SemanticDatepicker onDateChange={console.log} />
+    </Content>
+  ))
   .add('Basic with firstDayOfWeek', () => (
-    <SemanticDatepicker firstDayOfWeek={3} onDateChange={console.log} />
+    <Content>
+      <SemanticDatepicker firstDayOfWeek={3} onDateChange={console.log} />
+    </Content>
   ))
   .add('Basic with outside days', () => (
-    <SemanticDatepicker showOutsideDays onDateChange={console.log} />
+    <Content>
+      <SemanticDatepicker showOutsideDays onDateChange={console.log} />
+    </Content>
   ))
   .add('Basic with format prop', () => (
-    <SemanticDatepicker format="DD/MM/YYYY" onDateChange={console.log} />
+    <Content>
+      <SemanticDatepicker format="DD/MM/YYYY" onDateChange={console.log} />
+    </Content>
   ))
   .add('Range', () => (
-    <SemanticDatepicker type="range" onDateChange={console.log} />
+    <Content>
+      <SemanticDatepicker type="range" onDateChange={console.log} />
+    </Content>
+  ))
+  .add('Range with right pointing', () => (
+    <Content>
+      <SemanticDatepicker
+        type="range"
+        pointing="right"
+        onDateChange={console.log}
+      />
+    </Content>
   ))
   .add('Range with firstDayOfWeek', () => (
-    <SemanticDatepicker
-      type="range"
-      firstDayOfWeek={6}
-      onDateChange={console.log}
-    />
+    <Content>
+      <SemanticDatepicker
+        type="range"
+        firstDayOfWeek={6}
+        onDateChange={console.log}
+      />
+    </Content>
   ))
   .add('Range with outside days', () => (
-    <SemanticDatepicker
-      type="range"
-      showOutsideDays
-      onDateChange={console.log}
-    />
+    <Content>
+      <SemanticDatepicker
+        type="range"
+        showOutsideDays
+        onDateChange={console.log}
+      />
+    </Content>
   ))
   .add('Range with format prop', () => (
-    <SemanticDatepicker
-      type="range"
-      format="DD/MM/YYYY"
-      onDateChange={console.log}
-    />
+    <Content>
+      <SemanticDatepicker
+        type="range"
+        format="DD/MM/YYYY"
+        onDateChange={console.log}
+      />
+    </Content>
   ))
   .add('Basic with brazilian portuguese locale', () => (
-    <SemanticDatepicker
-      onDateChange={console.log}
-      format="DD/MM/YYYY"
-      locale={localePtBr}
-    />
+    <Content>
+      <SemanticDatepicker
+        onDateChange={console.log}
+        format="DD/MM/YYYY"
+        locale={localePtBr}
+      />
+    </Content>
   ))
   .add('Basic as form component', () => (
-    <Form>
-      <Form.Group width="equals">
-        <SemanticDatepicker
-          label="Birth date"
-          id="birthDate"
-          onDateChange={console.log}
-        />
-        <SemanticDatepicker
-          label="Start date"
-          id="startDate"
-          onDateChange={console.log}
-        />
-      </Form.Group>
-    </Form>
+    <Content>
+      <Form>
+        <Form.Group width="equals">
+          <SemanticDatepicker
+            label="Birth date"
+            id="birthDate"
+            onDateChange={console.log}
+          />
+          <SemanticDatepicker
+            label="Start date"
+            id="startDate"
+            onDateChange={console.log}
+          />
+        </Form.Group>
+      </Form>
+    </Content>
+  ))
+  .add('Basic with left pointing', () => (
+    <Content>
+      <SemanticDatepicker pointing="left" onDateChange={console.log} />
+    </Content>
+  ))
+  .add('Basic with right pointing', () => (
+    <Content>
+      <SemanticDatepicker pointing="right" onDateChange={console.log} />
+    </Content>
   ));
