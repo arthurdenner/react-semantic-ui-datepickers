c68de3d862811ca16ce06578e888b704c384e9b9
Arthur Denner <arthurdenner7@gmail.com>
2019-07-31 16:00:53 -0300

    refactor: Start rewrite of the library with tsdx

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+type SemanticDatePickerProps = {};
+
+export const DatePicker: React.FC<SemanticDatePickerProps> = () => {
+  return <div>This will be a datepicker</div>;
+};
