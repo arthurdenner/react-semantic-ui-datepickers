e19912212ccfe235c638e0ebfbd3ef643555dd64
Arthur Denner <arthurdenner7@gmail.com>
2019-10-01 15:51:49 +0200

    build: fix lint script, Jest config and add validate script

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    'react-app',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    '@typescript-eslint/consistent-type-assertions': 0,
+  },
+};
