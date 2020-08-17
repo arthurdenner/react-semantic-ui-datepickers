6d18c56135d5831a3355c63398727b8c11c97b71
Arthur Denner <arthurdenner7@gmail.com>
2020-08-17 18:09:41 +0200

    fix(datepicker): fire onChange callback when typed date is invalid (#336)
    * test: move setup function to new file
    
    * fix(datepicker): fire onChange callback when typed date is invalid
    
    * test(datepicker): fix test for usecase "onChange is fired when invalid date is typed"

---
+++ src/__tests__/usecases.test.tsx
@@ -0,0 +1,23 @@
+import { fireEvent, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { setup } from './_utils';
+
+it('onChange is fired when invalid date is typed', () => {
+  const onBlur = jest.fn();
+  const onChange = jest.fn();
+  const { datePickerInput, openDatePicker } = setup({ onBlur, onChange });
+
+  openDatePicker();
+  fireEvent.click(screen.getByText('Today'));
+
+  expect(onChange).toHaveBeenCalledTimes(1);
+  expect(onBlur).toHaveBeenCalledTimes(1);
+
+  userEvent.type(datePickerInput, '{backspace}');
+  userEvent.type(datePickerInput, '{backspace}');
+  fireEvent.keyDown(datePickerInput, { keyCode: 13 });
+
+  expect(datePickerInput.value).toBe('');
+  expect(onBlur).toHaveBeenCalledTimes(2);
+  expect(onChange).toHaveBeenCalledTimes(2);
+});
