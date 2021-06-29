1be27324ef68f2ff335d4a5d0d459dc21c2a33f4
Arthur Denner <arthurdenner7@gmail.com>
2021-06-29 19:26:17 +0000

    chore: fix Storybook and Babel config (#610)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
   stories: ['../stories/**/*.stories.tsx'],
   addons: ['@storybook/addon-knobs', '@storybook/addon-storysource'],
+  babel: async options => ({
+    ...options,
+    plugins: options.plugins.concat('@babel/plugin-proposal-class-properties'),
+  }),
 };
