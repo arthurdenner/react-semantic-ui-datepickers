7257e599b96ce45326fd37a6c4591fa47357f006
Arthur Denner <arthurdenner7@gmail.com>
2021-07-01 09:11:11 +0200

    chore: remove custom Babel config from .storybook/main

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,4 @@
 module.exports = {
   stories: ['../stories/**/*.stories.tsx'],
   addons: ['@storybook/addon-knobs', '@storybook/addon-storysource'],
-  babel: async options => ({
-    ...options,
-    plugins: options.plugins.concat('@babel/plugin-proposal-class-properties'),
-  }),
 };
